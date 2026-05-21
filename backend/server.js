a9ac9ecb92a55458f6a46d7a31222af9cfb973e2
musharraff <musharraff2018@gmail.com>
2026-05-21 14:58:57 +0100

    fix3 CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:5500',
   process.env.FRONTEND_URL,
-  'https://csc300l-project.vercel.app', // add your exact Vercel URL here
+  'https://fsd-ten-delta.vercel.app', // add your exact Vercel URL here
 ].filter(Boolean);
 
 app.use(cors({
